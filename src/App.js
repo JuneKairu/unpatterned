bf5cbc71fd1706de72b45225d05a397fa7268f42
JKairu <micadario0926@gmail.com>
2024-09-21 13:22:43 +0800

    fixed directory structures

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import Home from './components/Home';
 import backgroundImage from './assets/images/background2.jpg';
 
