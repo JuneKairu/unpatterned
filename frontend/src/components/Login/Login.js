0266dc35a566ae99255e2474604131c8813f254a
kawatan123 <09xghagox09@gmail.com>
2024-10-01 23:55:56 +0800

    teest

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+//test
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
