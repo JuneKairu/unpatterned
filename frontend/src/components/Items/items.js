589de3dfe6b0d7382ae5b9de672af48524b5783a
JKairu <micadario0926@gmail.com>
2024-10-04 08:36:22 +0800

    heh

--- frontend/src/components/Items/items.js
+++ frontend/src/components/Items/items.js
@@ -1,50 +1,84 @@
 export const categories = {
-    uniforms: [
-      { name: 'SBIT Departmental Shirt',
-          options: [
-            { name: 'S', price: 500},
-            { name: 'M', price: 600},
-            { name: 'L', price: 750},
-            { name: 'XL', price: 750},
-            { name: 'XXL', price: 750}
-          ] },
-      { name: 'SHTM Departmental Shirt', price: 550 },
-      { name: 'SSLATE Departmental Shirt', price: 750 },
-      { name: 'SARFAID Departmental Shirt', price: 500 },
-      { name: 'PE Uniform (College)', price: 600 },
-    ],
-    schoolsupplies: [
-      { name: 'Yellow Pad', price: 75 },
-      { name: 'Intermediate Pad', price: 75 },
-      { name: 'Eraser', price: 20 },
-      { name: 'Sharpener', price: 10 },
-      { name: 'Bond Paper',
-        options: [
-          { name: 'A4', price: 2 },
-          { name: 'Long', price: 2 },
-          { name: 'Short', price: 1 }
-        ]
-      }
-    ],
-    lccbmerchandise: [
-      { name: 'Jacket',
-        options: [
-          { name: 'White', price: 750},
-          { name: 'Blue', price: 750}
-        ]
-      },
-      { name: 'Baseball Cap', price: 250 },
-      { name: 'T-Shirt',
-        options: [
-          { name: 'White', price: 250},
-          { name: 'Blue', price: 250}
-        ]
-      },
-    ],
-    others: [
-      { name: 'Sanitary Pads', price: 10 },
-      { name: 'Thumb Stacks (Pack)', price: 50 },
-      { name: 'Paper Clips (Pack)', price: 20 },
-    ],
-  };
-  
+  uniforms: [
+    { name: 'SBIT Departmental Shirt', 
+      options: [
+        { name: 'XS', price: 500 },
+        { name: 'S', price: 500 },
+        { name: 'M', price: 500 },
+        { name: 'L', price: 500 },
+        { name: 'XL', price: 500 },
+        { name: 'XXL', price: 500 },
+      ]
+    },
+    { name: 'SHTM Departmental Shirt', 
+      options: [
+        { name: 'XS', price: 500 },
+        { name: 'S', price: 500 },
+        { name: 'M', price: 500 },
+        { name: 'L', price: 500 },
+        { name: 'XL', price: 500 },
+        { name: 'XXL', price: 500 },
+      ]
+    },
+    { name: 'SSLATE Departmental Shirt', 
+      options: [
+        { name: 'XS', price: 700 },
+        { name: 'S', price: 700 },
+        { name: 'M', price: 700 },
+        { name: 'L', price: 700 },
+        { name: 'XL', price: 700 },
+        { name: 'XXL', price: 700 },
+      ]
+    },
+    { name: 'SARFAID Departmental Shirt', 
+      options: [
+        { name: 'XS', price: 500 },
+        { name: 'S', price: 500 },
+        { name: 'M', price: 500 },
+        { name: 'L', price: 500 },
+        { name: 'XL', price: 500 },
+        { name: 'XXL', price: 500 },
+      ]
+    },
+    { name: 'PE Uniform (College)', 
+      options: [
+        { name: 'Shirt', price: 300},
+        { name: 'Pants', price: 300 }
+      ]
+    },
+  ],
+  schoolsupplies: [
+    { name: 'Yellow Pad', price: 75 },
+    { name: 'Intermediate Pad', price: 75 },
+    { name: 'Eraser', price: 15 },
+    { name: 'Sharpener', price: 10 },
+    { 
+      name: 'Bond Paper',
+      options: [
+        { name: 'A4', price: 2 },
+        { name: 'Long', price: 2 },
+        { name: 'Short', price: 1 },
+      ]
+    },
+  ],
+  lccbmerchandise: [
+    { name: 'Jacket',
+      options: [
+        { name: 'White', price: 750 },
+        { name: 'Blue', price: 750 },
+      ]
+    },
+    { name: 'Baseball Cap', price: 250 },
+    { name: 'T-Shirt',
+      options: [
+        { name: 'White', price: 250 },
+        { name: 'Blue', price: 250 },
+      ]
+    },
+  ],
+  others: [
+    { name: 'Sanitary Pads', price: 10 },
+    { name: 'Thumb Stacks (Pack)', price: 50 },
+    { name: 'Paper Clips (Pack)', price: 20 },
+  ],
+};
