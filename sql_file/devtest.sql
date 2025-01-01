8ec35b1221e8fb0659f563bf4b95aa38d82d63ea
Chris-G <09xghagox09@gmail.com>
2025-01-01 16:09:02 +0800

    new sql file

--- sql_file/devtest.sql
+++ sql_file/devtest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2024 at 08:02 PM
+-- Generation Time: Jan 01, 2025 at 09:08 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,10 +34,19 @@ CREATE TABLE `tbl_deliveries` (
   `supplier` varchar(255) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
   `total_amount` decimal(10,2) NOT NULL,
-  `contact_number` varchar(15) NOT NULL
+  `contact_number` varchar(15) NOT NULL,
+  `cost_price` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `selling_price` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_deliveries`
+--
+
+INSERT INTO `tbl_deliveries` (`id`, `delivery_date`, `delivery_time`, `supplier`, `product_id`, `quantity`, `total_amount`, `contact_number`, `cost_price`, `selling_price`) VALUES
+(10, '2025-01-09', '18:59:00', '213', 22, 100, 1000.00, '123121', 10.00, 10.00),
+(11, '2025-01-04', '17:01:00', 'national bookstore', 2, 100, 400.00, '09231321', 4.00, 7.00);
 
 -- --------------------------------------------------------
 
@@ -60,8 +69,7 @@ INSERT INTO `tbl_productcategory` (`category_id`, `category_name`, `created_at`)
 (3, 'Uniforms', '2024-11-14 13:21:07'),
 (4, 'Merchandise', '2024-11-14 14:24:56'),
 (6, 'Hair/Other Accessories', '2024-12-06 06:34:52'),
-(7, 'Miscellaneous', '2024-12-06 08:06:32'),
-(8, 'waay lng ', '2024-12-25 18:59:37');
+(7, 'Miscellaneous', '2024-12-06 08:06:32');
 
 -- --------------------------------------------------------
 
@@ -83,19 +91,40 @@ CREATE TABLE `tbl_products` (
 --
 
 INSERT INTO `tbl_products` (`product_id`, `category_id`, `product_name`, `price`, `quantity`, `created_at`) VALUES
-(2, 2, 'Ballpen', 7.00, 72, '2024-11-09 16:28:55'),
-(4, 2, 'pencil', 123.00, 100, '2024-11-10 14:34:55'),
+(2, 2, 'Ballpen', 7.00, 99, '2024-11-09 16:28:55'),
+(4, 2, 'pencil', 123.00, 99, '2024-11-10 14:34:55'),
 (5, 3, 'College P.E Uniform Small', 450.00, 100, '2024-11-14 13:22:17'),
-(7, 4, 'Lcc cap', 150.00, 96, '2024-11-14 14:25:11'),
-(16, 3, 'SHS P.E Unform XL', 450.00, 50, '2024-12-06 05:32:29'),
-(17, 6, 'Hair Clamp (large)', 25.00, 19, '2024-12-06 06:35:37'),
-(18, 6, 'Hair Pins', 3.00, 194, '2024-12-06 06:36:26'),
-(19, 6, 'Hairnet', 10.00, 50, '2024-12-06 06:37:17'),
-(20, 2, 'Yellowpad Roll', 50.00, 17, '2024-12-06 06:38:55'),
-(21, 2, 'Yellowpad paper 1', 1.00, 176, '2024-12-06 06:40:01'),
-(22, 3, 'SBIT Departmental T-shirt medium', 600.00, 20, '2024-12-06 06:41:41'),
-(23, 7, 'Thumbstacks Box', 20.00, 99, '2024-12-06 08:07:20'),
-(24, 8, '213da', 123.00, 11, '2024-12-25 18:59:55');
+(7, 4, 'Lcc cap', 150.00, 95, '2024-11-14 14:25:11'),
+(16, 3, 'SHS P.E Unform XL', 500.00, 48, '2024-12-06 05:32:29'),
+(17, 6, 'Hair Clamp (large)', 25.00, 13, '2024-12-06 06:35:37'),
+(18, 6, 'Hair Pins', 3.00, 192, '2024-12-06 06:36:26'),
+(19, 6, 'Hairnet', 10.00, 18, '2024-12-06 06:37:17'),
+(20, 2, 'Yellowpad Roll', 50.00, 16, '2024-12-06 06:38:55'),
+(21, 2, 'Yellowpad paper 1', 1.00, 175, '2024-12-06 06:40:01'),
+(22, 3, 'SBIT Departmental T-shirt medium', 600.00, 19, '2024-12-06 06:41:41'),
+(23, 7, 'Thumbstacks Box', 20.00, 89, '2024-12-06 08:07:20'),
+(25, 2, 'Panda ballben', 10.00, 97, '2024-12-27 06:12:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_request_password`
+--
+
+CREATE TABLE `tbl_request_password` (
+  `request_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `request_date` datetime NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_request_password`
+--
+
+INSERT INTO `tbl_request_password` (`request_id`, `email`, `request_date`, `status`, `created_at`) VALUES
+(1, 'user@gmail.com', '2024-12-27 13:33:29', 'pending', '2024-12-27 05:33:29');
 
 -- --------------------------------------------------------
 
@@ -108,16 +137,22 @@ CREATE TABLE `tbl_stock_requests` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `status` varchar(20) NOT NULL,
-  `request_date` datetime NOT NULL
+  `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_stock_requests`
 --
 
-INSERT INTO `tbl_stock_requests` (`request_id`, `product_id`, `quantity`, `status`, `request_date`) VALUES
-(1, 4, 20, 'pending', '2024-12-26 00:37:04'),
-(2, 4, 100, 'pending', '2024-12-26 01:13:05');
+INSERT INTO `tbl_stock_requests` (`request_id`, `product_id`, `quantity`, `status`, `created_at`) VALUES
+(1, 4, 20, 'completed', '2024-12-27 10:22:41'),
+(2, 4, 100, 'completed', '2024-12-27 10:22:41'),
+(3, 22, 20, 'completed', '2024-12-27 10:22:41'),
+(4, 20, 9, 'completed', '2024-12-27 10:22:41'),
+(5, 20, 2, 'completed', '2024-12-27 10:22:50'),
+(6, 17, 10, 'completed', '2024-12-27 10:23:58'),
+(7, 19, 123, 'completed', '2024-12-27 12:58:10'),
+(8, 25, 100, 'completed', '2024-12-27 14:14:13');
 
 -- --------------------------------------------------------
 
@@ -136,19 +171,12 @@ CREATE TABLE `tbl_transactions` (
 --
 
 INSERT INTO `tbl_transactions` (`transaction_id`, `created_at`, `total_amount`) VALUES
-('T1732690389595', '2024-12-01 06:53:09', 1107.00),
-('T1733159959666', '2024-12-02 17:19:19', 307.00),
-('T1733170187295', '2024-12-02 20:09:47', 280.00),
-('T1733326545835', '2024-12-04 15:35:45', 84.00),
-('T1733414212357', '2024-12-05 15:56:52', 2337.00),
-('T1733460819827', '2024-12-06 04:53:39', 140.00),
-('T1733463022596', '2024-12-06 05:30:22', 369.00),
-('T1733468714673', '2024-12-06 07:05:14', 23.00),
-('T1733472521493', '2024-12-06 08:08:41', 50.00),
-('T1733473081132', '2024-12-06 08:18:01', 38.00),
-('T1735144733625', '2024-12-25 16:38:53', 42.00),
-('T1735145873129', '2024-12-25 16:57:53', 600.00),
-('T1735146745411', '2024-12-25 17:12:25', 123.00);
+('T1735711180899', '2025-01-01 05:59:40', 38.00),
+('T1735711217902', '2025-01-01 06:00:17', 146.00),
+('T1735711277065', '2025-01-01 06:01:17', 15.00),
+('T1735711333510', '2025-01-01 06:02:13', 10.00),
+('T1735718757120', '2025-01-01 08:05:57', 450.00),
+('T1735718820975', '2025-01-01 08:07:00', 500.00);
 
 -- --------------------------------------------------------
 
@@ -161,35 +189,26 @@ CREATE TABLE `tbl_transaction_details` (
   `transaction_id` varchar(50) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL
+  `price` decimal(10,2) DEFAULT NULL,
+  `price_at_time` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbl_transaction_details`
 --
 
-INSERT INTO `tbl_transaction_details` (`id`, `transaction_id`, `product_id`, `quantity`, `price`) VALUES
-(12, 'T1732690389595', 4, 9, NULL),
-(13, 'T1733159959666', 7, 2, NULL),
-(14, 'T1733159959666', 2, 1, NULL),
-(15, 'T1733170187295', 7, 1, NULL),
-(16, 'T1733170187295', 2, 1, NULL),
-(17, 'T1733170187295', 4, 1, NULL),
-(18, 'T1733326545835', 2, 12, NULL),
-(19, 'T1733414212357', 4, 19, NULL),
-(20, 'T1733460819827', 2, 20, NULL),
-(21, 'T1733463022596', 4, 3, NULL),
-(22, 'T1733468714673', 18, 5, NULL),
-(23, 'T1733468714673', 2, 1, NULL),
-(24, 'T1733468714673', 21, 1, NULL),
-(25, 'T1733472521493', 20, 1, NULL),
-(26, 'T1733473081132', 2, 1, NULL),
-(27, 'T1733473081132', 21, 3, NULL),
-(28, 'T1733473081132', 18, 1, NULL),
-(29, 'T1733473081132', 17, 1, NULL),
-(30, 'T1735144733625', 2, 6, NULL),
-(31, 'T1735145873129', 20, 12, NULL),
-(32, 'T1735146745411', 4, 1, NULL);
+INSERT INTO `tbl_transaction_details` (`id`, `transaction_id`, `product_id`, `quantity`, `price`, `price_at_time`) VALUES
+(1, 'T1735711180899', 17, 1, NULL, 25.00),
+(2, 'T1735711180899', 18, 1, NULL, 3.00),
+(3, 'T1735711180899', 19, 1, NULL, 10.00),
+(4, 'T1735711217902', 2, 1, NULL, 7.00),
+(5, 'T1735711217902', 25, 1, NULL, 15.00),
+(6, 'T1735711217902', 4, 1, NULL, 123.00),
+(7, 'T1735711217902', 21, 1, NULL, 1.00),
+(8, 'T1735711277065', 25, 1, NULL, 15.00),
+(9, 'T1735711333510', 25, 1, NULL, 10.00),
+(10, 'T1735718757120', 16, 1, NULL, 450.00),
+(11, 'T1735718820975', 16, 1, NULL, 500.00);
 
 -- --------------------------------------------------------
 
@@ -220,7 +239,9 @@ INSERT INTO `tb_logins` (`id`, `email`, `password`) VALUES
 --
 ALTER TABLE `tbl_deliveries`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`);
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `idx_delivery_date` (`delivery_date`),
+  ADD KEY `idx_supplier` (`supplier`);
 
 --
 -- Indexes for table `tbl_productcategory`
@@ -234,6 +255,13 @@ ALTER TABLE `tbl_productcategory`
 ALTER TABLE `tbl_products`
   ADD PRIMARY KEY (`product_id`),
   ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `tbl_request_password`
+--
+ALTER TABLE `tbl_request_password`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `idx_email` (`email`);
 
 --
 -- Indexes for table `tbl_stock_requests`
@@ -270,7 +298,7 @@ ALTER TABLE `tb_logins`
 -- AUTO_INCREMENT for table `tbl_deliveries`
 --
 ALTER TABLE `tbl_deliveries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tbl_productcategory`
@@ -282,19 +310,25 @@ ALTER TABLE `tbl_productcategory`
 -- AUTO_INCREMENT for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `tbl_request_password`
+--
+ALTER TABLE `tbl_request_password`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_stock_requests`
 --
 ALTER TABLE `tbl_stock_requests`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_transaction_details`
 --
 ALTER TABLE `tbl_transaction_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tb_logins`
